129d7220d54397271e48c1ce65ebf66fa78e77d9
Web Ghost <108879264+HAWKZ4@users.noreply.github.com>
2024-04-11 14:32:30 +0300

    Feat(Post): - Define M:N association between 'Post'and 'Category' models (Posts belong to many Categories, and Categories belong to many Posts)
    - Define 1:M association between 'Post' and 'User' models (Post belongs to just one user)

--- models/post.js
+++ models/post.js
@@ -8,7 +8,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Post.belongsToMany(sequelize.define("Category"), { through: "PostCategories" });
+      Post.belongsTo(sequelize.define("User"))
+      
     }
   }
   Post.init(
